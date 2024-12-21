c0be51c5d52096bb50e8367dc9a8071ee129d37a
Billy <billydevbusiness@gmail.com>
2024-12-21 16:53:09 +0530

    feat: Basic support for bindings and environment

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"cinnamon/evaluator"
 	"cinnamon/lexer"
+	"cinnamon/object"
 	"cinnamon/parser"
 	"fmt"
 	"io"
@@ -13,6 +14,8 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
+
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
@@ -31,7 +34,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
